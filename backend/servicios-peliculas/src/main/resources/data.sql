4ee511f0cb5b6fd29fda6aa2053dda4bdafd8bf9
Diego <di.argomedo@duocuc.cl>
2025-12-08 15:41:45 -0300

    Mejoras al POM 2

---
+++ backend/servicios-peliculas/src/main/resources/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO pelicula (titulo, descripcion, genero, duracion, anio, imagen) VALUES
+('Joker', 'Un comediante fallido...', 'Acción', '2h 2m', 2019, 'https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg'),
+('Kill Bill', 'Una asesina se despierta...', 'Acción', '1h 51m', 2003, 'https://image.tmdb.org/t/p/w500/v7TaX8kXMXs5yFFGR41guUDNcnB.jpg'),
+('Matrix', 'Un experto en computadoras...', 'Acción', '2h 16m', 1999, 'https://image.tmdb.org/t/p/w500/aOIuZAjPaRIE6CMzbazvcHuHXDc.jpg'),
+('John Wick', 'Un ex asesino a sueldo...', 'Acción', '1h 41m', 2014, 'https://image.tmdb.org/t/p/w500/fZPSd91yGE9fCcCe6OoQr6E3Bev.jpg'),
+
+('Loco por Mary', 'Un hombre enamorado...', 'Comedia', '1h 42m', 2003, 'https://image.tmdb.org/t/p/w500/dSOfZ9Ni0Nd7dAXR2CT2JI4ljsl.jpg'),
+('Irene, yo y mi otro yo', 'Un policía con doble personalidad...', 'Comedia', '1h 56m', 2000, 'https://image.tmdb.org/t/p/w500/9Jx2FzdKUYqF3BojG65xjqIG9aj.jpg'),
+('Scary Movie', 'Parodia del cine slasher...', 'Comedia', '1h 28m', 2000, 'https://image.tmdb.org/t/p/w500/fHWR3YplPQWciYzxEity2kyDoDn.jpg'),
+('Supercool', 'Dos estudiantes buscan...', 'Comedia', '1h 59m', 2007, 'https://image.tmdb.org/t/p/w500/ftUDFRYywWzajElI2RT9NIno2PS.jpg'),
+
+('Lost in Translation', 'Una amistad inusual...', 'Romance', '1h 42m', 2003, 'https://image.tmdb.org/t/p/w500/ybXq8xTs6VMx3hM4RDYMXaDZwcz.jpg'),
+('Cuestión de Tiempo', 'Tim Lake descubre...', 'Romance', '2h 3m', 2013, 'https://image.tmdb.org/t/p/w500/6b8zipp2Z4Io5RVuaATBNKtlbE6.jpg'),
+('500 días con ella', 'Tom recuerda su relación...', 'Romance', '1h 42m', 2009, 'https://image.tmdb.org/t/p/w500/xziurNdvxWZkgonZ5ZaGRB9YdLt.jpg'),
+('Orgullo y Prejuicio', 'Elizabeth Bennet conoce...', 'Romance', '2h 8m', 2005, 'https://image.tmdb.org/t/p/w500/oixzLjpyaLagLa8UREts1NiHr6F.jpg'),
+
+('Eterno Resplandor...', 'Una pareja borra recuerdos...', 'Drama', '1h 48m', 2004, 'https://image.tmdb.org/t/p/w500/5MwkWH9tYHv3mV9OdYTMR5qreIz.jpg'),
+('Réquiem por un sueño', 'Una viuda cae en adicción...', 'Drama', '1h 42m', 2000, 'https://image.tmdb.org/t/p/w500/nOd6vjEmzCT0k4VYqsA2hwyi87C.jpg'),
+('American History X', 'Exneonazi intenta cambiar...', 'Drama', '1h 59m', 1998, 'https://image.tmdb.org/t/p/w500/h2cDqHvnZkycBJKoF7WhcQ2MX1V.jpg'),
+('Sueños de Fuga', 'Un hombre inocente va a prisión...', 'Drama', '2h 2m', 1994, 'https://image.tmdb.org/t/p/w500/lyQBXzOQSuE59IsHyhrp0qIiPAz.jpg');
